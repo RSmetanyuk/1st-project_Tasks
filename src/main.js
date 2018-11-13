98d53ae7ed2f9aea9aa85b4747c78f76e9e5aee2
RSmetanyuk <rsmetanyuk@gmail.com>
2018-11-14 01:43:58 +0200

    bootstrap with glyphicon bug fixed

--- src/main.js
+++ src/main.js
@@ -15,25 +15,34 @@ const NowMoment = moment();
   taskType: 1,
   deleatet: false}];
 */
-const $menueButtons = document.getElementsByClassName('footer-button');
-const $buttonNew = $menueButtons[0];
+const $$menueButtons = document.getElementsByClassName('footer-button');
+const $buttonNew = $$menueButtons[0];
 const $taskEditButtonBack = document.getElementsByClassName('task-edit_button-back')[0];
+const mainWiewingDay = NowMoment;
+//const milisecInDay = 8.64e+7;
 
-document.getElementsByClassName('day-header__date')[0].innerHTML = NowMoment.format('D MMM YYYY');
-document.getElementsByClassName('day-header__week')[0].innerHTML = `${NowMoment.format('Wo')} week`;
+
+document.getElementsByClassName('day-header__date')[0].innerHTML = 
+  mainWiewingDay.format('D MMM YYYY');
+document.getElementsByClassName('day-header__week')[0].innerHTML = 
+  `${mainWiewingDay.format('Wo')} week`;
 document.getElementsByClassName('task__task-date')[0].innerHTML = NowMoment.format('HH : MM');
 
+const buttonNewOnClick = () => {
+  document.getElementsByClassName('task-edit')[0].classList.toggle('hidden');
+  document.getElementsByClassName('main-view')[0].classList.toggle('hidden');
+  $buttonNew.setAttribute('disabled', '');
+};
+
+const taskEditButtonBackOnClick = () => {
+  document.getElementsByClassName('task-edit')[0].classList.toggle('hidden');
+  document.getElementsByClassName('main-view')[0].classList.toggle('hidden');
+  $buttonNew.removeAttribute('disabled');
+};
+
 const init = () => {
-  $buttonNew.addEventListener('click', () => {
-    document.getElementsByClassName('task-edit')[0].classList.toggle('hidden');
-    document.getElementsByClassName('main-view')[0].classList.toggle('hidden');
-    $buttonNew.setAttribute('disabled', '');
-  });
-  $taskEditButtonBack.addEventListener('click', () => {
-    document.getElementsByClassName('task-edit')[0].classList.toggle('hidden');
-    document.getElementsByClassName('main-view')[0].classList.toggle('hidden');
-    $buttonNew.removeAttribute('disabled');
-  });
+  $buttonNew.addEventListener('click', buttonNewOnClick);
+  $taskEditButtonBack.addEventListener('click', taskEditButtonBackOnClick);
 };
 
 init();
