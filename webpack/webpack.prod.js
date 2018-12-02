40ae4ffe3a3964346a1d4a4539711b121c3cd3cc
RSmetanyuk <rsmetanyuk@gmail.com>
2018-12-02 02:53:13 +0200

    test with assets

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist')
+    path: path.resolve(__dirname, '../assets')
   },
   devtool: 'cheap-module-eval-source-map',
   module: {
